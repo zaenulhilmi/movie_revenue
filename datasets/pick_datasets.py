4adcd2050986da915e8c3f49471f1148cad1a04e
Zaenul Hilmi <zaenulhilmi@gmail.com>
2020-04-03 00:23:13 +0700

    don't really know what am I doing but it seems like working -_-'

--- datasets/pick_datasets.py
+++ datasets/pick_datasets.py
@@ -2,6 +2,6 @@ import pandas as pd
 
 raw_dataset = pd.read_csv('./tmdb_5000_movies.csv')
 
-use_dataset = raw_dataset[['revenue', 'title', 'popularity', 'budget', 'runtime', 'vote_average', 'vote_count']]
+use_dataset = raw_dataset[['revenue', 'popularity', 'budget', 'runtime', 'vote_average', 'vote_count']]
 
 use_dataset.to_csv('./movies.csv', index=False)
--- index.py
+++ index.py
@@ -7,10 +7,9 @@ import seaborn as sns
 
 import tensorflow as tf
 
+
 from tensorflow import keras
 from tensorflow.keras import layers
-
-# print(tf.__version__)
 
 
 def build_model():
@@ -27,48 +26,70 @@ def build_model():
                   metrics=['mae', 'mse'])
     return model
 
+
 def norm(x):
   return (x - train_stats['mean']) / train_stats['std']
-# import tensorflow_docs as tfdocs
-# import tensorflow_docs.plots
+
+import tensorflow_docs as tfdocs
+import tensorflow_docs.plots
 # import tensorflow_docs.modeling
 
-# column_names = ['MPG','Cylinders','Displacement','Horsepower','Weight',
-#                 'Acceleration', 'Model Year', 'Origin']
-raw_dataset = pd.read_csv('./datasets/tmdb_5000_movies.csv')
 
+raw_dataset = pd.read_csv('./datasets/movies.csv')
 dataset = raw_dataset.copy()
-dataset.tail()
 
+
+#
 train_dataset = dataset.sample(frac=0.8,random_state=0)
 test_dataset = dataset.drop(train_dataset.index)
+#
 
-sns.pairplot(train_dataset[["budget", "revenue", "vote_count"]], diag_kind="kde")
+# sns.pairplot(train_dataset[['revenue', 'vote_average', 'vote_count']], diag_kind="kde")
+# plt.show()
 
 train_stats = train_dataset.describe()
 train_stats.pop("revenue")
 train_stats = train_stats.transpose()
 
-print(train_stats['mean'])
 
-# normed_train_data = norm(train_dataset)
-# normed_test_data = norm(test_dataset)
-# print(normed_train_data)
+normed_train_data = norm(train_dataset)
+normed_test_data = norm(test_dataset)
 
 train_labels = train_dataset.pop('revenue')
 test_labels = test_dataset.pop('revenue')
 
 model = build_model()
 
-# model.summary()
 
+# example_batch = normed_train_data[:10]
+# print(example_batch.ndim)
+# # revenue,popularity,budget,runtime,vote_average,vote_count
+# example_result = model.predict(example_batch[['popularity', 'budget', 'runtime', 'vote_average', 'vote_count']])
+# print(example_result)
 
-example_batch = train_dataset[:10]
-example_result = model.predict(example_batch)
-print(example_result)
+EPOCHS = 1000
 
+history = model.fit(
+  normed_train_data[['popularity', 'budget', 'runtime', 'vote_average', 'vote_count']][:2000], train_labels[:2000],
+  epochs=EPOCHS, validation_split = 0.2, verbose=0)
 
+hist = pd.DataFrame(history.history)
+hist['epoch'] = history.epoch
+print(hist.tail())
 
+loss, mae, mse = model.evaluate(normed_test_data[['popularity', 'budget', 'runtime', 'vote_average', 'vote_count']], test_labels, verbose=2)
+print(loss, mae, mse)
+test_predictions = model.predict(normed_test_data[['popularity', 'budget', 'runtime', 'vote_average', 'vote_count']]).flatten()
+
+a = plt.axes(aspect='equal')
+plt.scatter(test_labels, test_predictions)
+plt.xlabel('True Values [revenue]')
+plt.ylabel('Predictions [revenue]')
+lims = [0, 1000000000]
+plt.xlim(lims)
+plt.ylim(lims)
+_ = plt.plot(lims, lims)
+plt.show()
 
 
 
